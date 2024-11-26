593eaf31155ea5a78e5151bdb9b0c5eca4541993
rifat32 <rifatbilalphilips@gmail.com>
2024-11-26 19:37:32 +0600

    update

--- app/Http/Controllers/client/ClientBookingController.php
+++ app/Http/Controllers/client/ClientBookingController.php
@@ -1782,7 +1782,7 @@ class ClientBookingController extends Controller
      *         response=400,
      *         description="Bad request, validation error",
      *         @OA\JsonContent(
-     *             @OA\Property(property="message", type="string", example="The given data was invalid.")
+ 
      *         )
      *     ),
      *     @OA\Response(
@@ -1807,12 +1807,12 @@ class ClientBookingController extends Controller
      *     )
      * )
      */
-    public function releaseSlot(HoldSlotRequest $request)
+    public function releaseSlot(Request $request)
     {
         $request_data = $request->validated();
 
         $request_data["customer_id"] = auth()->user()->id;
-        $request_data["held_until"] = Carbon::now()->addSeconds(90);
+
 
         // Delete all slots for the customer, including expired ones
         SlotHold::where('customer_id', $request_data["customer_id"])
